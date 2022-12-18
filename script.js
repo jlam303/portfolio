11c47f17e7f41812ecd97c25f7d5a5d511190332
Sarc4sticNerd <jplam904@gmail.com>
2022-12-18 03:01:39 -0700

    Added main images, scaling, and fixed animations

--- script.js
+++ script.js
@@ -3,19 +3,28 @@ let beltOn = true;
 function pauseBelt(){
     if(beltOn == true) 
     {
+        document.getElementById("lever").src = "./images/Lever2.png"
         for(let i =1;i<=3;i++)
         {
             boxes = document.getElementById("box" + i)
             boxes.style.animationPlayState = "paused";
+            document.getElementById("downBelt1").style.backgroundImage = 'url("./images/ConveyorD1.png")';
+            document.getElementById("rightBelt").style.backgroundImage = 'url("./images/Conveyor1.png")';
+            document.getElementById("downBelt2").style.backgroundImage = 'url("./images/ConveyorD1.png")';
         }
     }
     else{
+        document.getElementById("lever").src = "./images/Lever1.png"
         for(let i =1;i<=3;i++)
         {
             boxes = document.getElementById("box" + i)
             boxes.style.animationPlayState = "running";
+            document.getElementById("downBelt1").style.backgroundImage = 'url("./images/ConveyorD.gif")';
+            document.getElementById("rightBelt").style.backgroundImage = 'url("./images/Conveyor.gif")';
+            document.getElementById("downBelt2").style.backgroundImage = 'url("./images/ConveyorD.gif")';
         }
     }
     if(beltOn == true) beltOn = false;
     else beltOn = true
+
 }
